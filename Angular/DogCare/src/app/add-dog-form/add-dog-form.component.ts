ec1f2a59511715a1782c48f033290dfe61ee05c7
Stupy18 <stupariubogdan@gmail.com>
2024-04-08 18:04:22 +0300

    Made a connection to dog api to have all dog breeds available and also auto fetch images.

--- Angular/DogCare/src/app/add-dog-form/add-dog-form.component.ts
+++ Angular/DogCare/src/app/add-dog-form/add-dog-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { DogService } from '../services/dog.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-add-dog-form',
@@ -10,8 +11,35 @@ export class AddDogFormComponent {
   @Output() dogAdded = new EventEmitter<any>();
   show = false; // Control visibility with this
   newDog = { name: '', breedName: '', age: 0, weight: 0, height: 0, imageURL: '' };
+  breeds: any[] = [];
 
-  constructor(private dogService: DogService) {}
+  constructor(private http: HttpClient,private dogService: DogService) {}
+
+  ngOnInit(): void {
+    this.fetchBreeds();
+    this.onBreedChange();
+  }
+
+  onBreedChange() {
+    const selectedBreed = this.breeds.find(breed => breed.name === this.newDog.breedName);
+    if (selectedBreed && selectedBreed.reference_image_id) {
+      // Construct the image URL directly from the reference_image_id
+      this.newDog.imageURL = `https://cdn2.thedogapi.com/images/${selectedBreed.reference_image_id}.jpg`;
+    } else {
+      // Reset the imageURL if no breed is selected or if the breed has no reference_image_id
+      this.newDog.imageURL = '';
+    }
+  }
+
+
+  fetchBreeds() {
+    this.http.get<any[]>('https://api.thedogapi.com/v1/breeds', {
+      headers: { 'x-api-key': 'live_StlZNPMyGC31fefMQBBrCEgXSc0hcVJbCM7O7UAGbbw5uMRj9DB2zAgpzho2mSkF' }
+    }).subscribe(breeds => {
+      this.breeds = breeds;
+      // Optionally set a default breed and image here
+    });
+  }
 
   openModal(): void { this.show = true; }
   closeModal(): void { this.show = false; }
